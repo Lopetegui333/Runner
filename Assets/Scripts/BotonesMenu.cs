bcb2dc423143808ac658ad8336522886f40e6558
lopetegui333 <fifalex10@gmail.com>
2018-11-30 20:25:00 +0100

    Primer commit

---
+++ Assets/Scripts/BotonesMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonesMenu : MonoBehaviour {
+
+    public Button BtStart, BtOptions, BtExit;
+
+	// Use this for initialization
+	void Start () {
+        BtStart.onClick.AddListener(play);
+        BtExit.onClick.AddListener(exit);
+        BtOptions.onClick.AddListener(options);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void exit()
+    {
+
+    }
+
+    void play()
+    {
+
+    }
+
+    void options()
+    {
+
+    }
+}

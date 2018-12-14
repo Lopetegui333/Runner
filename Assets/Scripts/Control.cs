4742f14d367757d99726462b86ffc0c4b9683b0f
lopetegui333 <fifalex10@gmail.com>
2018-12-14 20:13:17 +0100

    14/12/18

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+    Animator Animator;
+	// Use this for initialization
+	void Start () {
+        Animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Animator.SetBool("saltando", true);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Animator.SetBool("deslizando", true);
+        }
+    }
+    void RunAgain()
+    {
+        Animator.SetBool("saltando", false);
+    }
+    void RunAgainD()
+    {
+
+        Animator.SetBool("deslizando", false);
+
+    }
+}

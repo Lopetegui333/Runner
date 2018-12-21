9d382e584756e593c3ac5da2b9acc76f181541cb
lopetegui333 <fifalex10@gmail.com>
2018-12-21 20:23:36 +0100

    21/12/18 Pausa

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour {
+
+    public static string SCENE_MAIN = "Menu";
+    public static string SCENE_PLAY = "Play";
+    public static string SCENE_OPTIONS = "Options";
+
+    public static string ANIMATION_SALTANDO = "saltando";
+    public static string ANIMATION_AGACHADO = "deslizando";
+}

597dfbdb8a4c48e23a16c5f6bd1c5e9a0f604380
lopetegui333 <fifalex10@gmail.com>
2019-01-25 20:16:56 +0100

    25/1/19 Generation

---
+++ Assets/Scripts/Controlador.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : ControladorBasico {
+    public override void juegoEnPausa()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void juegoRestaurado()
+    {
+        throw new System.NotImplementedException();
+    }
+    public GameObject Plataforma1;
+    public GameObject Plataforma2;
+    public Transform puntoinicio;
+    public Transform puntoanclaje1;
+   
+    // Use this for initialization
+    void Start () {
+        base.Start();
+
+        var PInicio = Instantiate(Plataforma1, puntoinicio.transform.position, puntoinicio.transform.rotation);
+        var anclaje = PInicio.gameObject.transform.Find("anclaje1");
+        Instantiate(Plataforma2, anclaje.transform.position, anclaje.transform.rotation);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}

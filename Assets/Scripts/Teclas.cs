bcb115d53a665f095739aeea8f2fed5f57d3fcaa
lopetegui333 <fifalex10@gmail.com>
2019-03-29 20:09:18 +0100

    Menu 29/3/19

---
+++ Assets/Scripts/Teclas.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Teclas : MonoBehaviour
+{
+    public Button botonSalto;
+    public Button botonAgacharse;
+    public Button botonIzqd;
+    public Button botonDcha;
+    public Canvas panel;
+    public Canvas panel2;
+    private Boolean esperarATeclaDeSalto = false;
+    private Boolean esperarATeclaAgacharse = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        botonSalto.onClick.AddListener(jumpRebind);
+        botonAgacharse.onClick.AddListener(agacharseRebind);
+        panel.enabled = false;
+        panel2.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        botonSalto.GetComponentInChildren<Text>().text = ValorOpciones.JUMP_BUTTON.ToString();
+        botonAgacharse.GetComponentInChildren<Text>().text = ValorOpciones.AGACHARSE_BUTTON.ToString();
+        botonIzqd.GetComponentInChildren<Text>().text = ValorOpciones.IZQD_BUTTON.ToString();
+
+    }
+
+    void jumpRebind()
+    {
+        panel.enabled = true;
+        panel2.enabled = false;
+        esperarATeclaDeSalto = true;
+        
+    }
+    void agacharseRebind()
+    {
+        panel.enabled = true;
+        panel2.enabled = false;
+        esperarATeclaAgacharse = true;
+    }
+
+    private void OnGUI()
+    {
+        var evento = Event.current;
+        if (evento.isKey && esperarATeclaDeSalto)
+        {
+            Debug.Log("Pulsado: " + evento.keyCode);
+            ValorOpciones.JUMP_BUTTON = evento.keyCode;
+            esperarATeclaDeSalto = false;
+            panel.enabled = false;
+            panel2.enabled = true;
+        }
+        var evento2 = Event.current;
+        if (evento2.isKey && esperarATeclaAgacharse)
+        {
+            Debug.Log("Pulsado: " + evento.keyCode);
+            ValorOpciones.AGACHARSE_BUTTON = evento.keyCode;
+            esperarATeclaAgacharse = false;
+            panel.enabled = false;
+            panel2.enabled = true;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/ValorOpciones.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ValorOpciones
+{
+    public static KeyCode DEFAULT_JUMP_BUTTON = KeyCode.Space;
+
+    
+    public static KeyCode JUMP_BUTTON = DEFAULT_JUMP_BUTTON;
+
+    public static KeyCode DEFAULT_AGACHARSE_BUTTON = KeyCode.C;
+
+
+    public static KeyCode AGACHARSE_BUTTON = DEFAULT_AGACHARSE_BUTTON;
+}
